ea7c149961e8025f91dd011b2afac46271471813
Leonardo Pereyra <leofranco_97@outlook.com>
2023-12-07 16:38:24 -0500

    challenge 7's code improved

--- challenge-7/main.js
+++ challenge-7/main.js
@@ -1,23 +1,29 @@
 function drawGift(size, symbol) {
     if (size == 1) {
-        return "#\n";
+        return '#\n'
     }
 
-    let gift = "";
+    let gift = '';
     
-    gift += " ".repeat(size-1)+"#".repeat(size)+"\n";
+    gift += ' '.repeat(size-1)+'#'.repeat(size)+'\n';
     
     for (let i = 0; i < size-2; i++) {
-        gift += " ".repeat(size-2-i)+"#"+symbol.repeat(size-2)+"#"+symbol.repeat(i)+"#\n";
+        gift += ' '.repeat(size-2-i)
+        +'#'+symbol.repeat(size-2)
+        +'#'+symbol.repeat(i)+'#'
+        +'\n';
     }
     
-    gift += "#".repeat(size)+symbol.repeat(size-2)+"#\n";
+    gift += '#'.repeat(size)+symbol.repeat(size-2)+'#'+'\n';
 
     for (let i = 0; i < size-2; i++) {
-        gift += "#"+symbol.repeat(size-2)+"#"+symbol.repeat(size-3-i)+"#"+" ".repeat(size-2-i)+"\n";
+        gift += '#'+symbol.repeat(size-2)
+        +'#'+symbol.repeat(size-3-i)
+        +'#'
+        +'\n';
     }
 
-    gift += "#".repeat(size)+" ".repeat(size-1)+"\n";
+    gift += '#'.repeat(size)+'\n';
 
     return gift;
 }
